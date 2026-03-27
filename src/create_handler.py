5f8b83784e70692b6c4133f2f7adae52fc19cb57
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-27 08:20:16 +0200

    refactor: fetch and store threads

--- src/create_handler.py
+++ src/create_handler.py
@@ -3,23 +3,42 @@ import os
 import httpx
 
 from src import RedditClient
-from src.utils.str_utils import clean_subreddit
+from src.storer import S3Storer
+from src.utils.date_utils import Today
+from src.utils.s3 import S3, Partition
 from src.logger import logger
+from src.utils.str_utils import clean_subreddit
 
 PROXY = os.getenv("HTTP_PROXY")
+BUCKET = os.getenv("BUCKET")
 
 
 def create_handler():
     def handler(event: dict, context: dict):
         subreddit = clean_subreddit(event["subreddit"])
+        if not BUCKET:
+            raise ValueError("No bucket name provided")
+
+        s3 = S3(bucket=BUCKET)
+        storer = S3Storer(s3=s3)
+        today = Today()
 
         with httpx.Client(timeout=30, proxy=PROXY) as client:
             client = RedditClient(client=client)
+
             try:
-                return [
-                    t.serialized
-                    for t in client.fetch_threads_for_subreddit(subreddit=subreddit)
-                ]
+                threads = client.fetch_threads_for_subreddit(subreddit=subreddit)
+                for thread in threads:
+                    storer.store(
+                        partition=Partition(
+                            subreddit=subreddit,
+                            year=today.year,
+                            month=today.month,
+                            day=today.day,
+                        ),
+                        data=thread,
+                    )
+
             except Exception as e:
                 logger.exception(str(e))
 
