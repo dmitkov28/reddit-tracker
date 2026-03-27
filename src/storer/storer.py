1177273e9dbe85159369e91a55660bc9df665e1b
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-27 14:21:24 +0200

    refactor: update store method to accept both Thread and Comment data types

--- src/storer/storer.py
+++ src/storer/storer.py
@@ -1,16 +1,24 @@
-from typing import Protocol
+from typing import Protocol, Union
 
-from src.model import ThreadClean
-from src.utils.s3 import S3, Partition
+from src.model import CommentList, ThreadClean
+from src.utils.s3 import S3, CommentPartition, ThreadPartition
 
 
 class Storer(Protocol):
-    def store(self, partition: Partition, data: ThreadClean): ...
+    def store(
+        self,
+        partition: Union[ThreadPartition, CommentPartition],
+        data: Union[ThreadClean, CommentList],
+    ): ...
 
 
 class S3Storer(Storer):
     def __init__(self, s3: S3):
         self._s3 = s3
 
-    def store(self, partition: Partition, data: ThreadClean):
+    def store(
+        self,
+        partition: Union[ThreadPartition, CommentPartition],
+        data: Union[ThreadClean, CommentList],
+    ):
         self._s3.put(key=str(partition), data=data.serialized)
