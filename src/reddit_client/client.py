7a4b233882a2ac5773ad410d810e6bf351dc108b
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-27 14:21:34 +0200

    feat: add fetch comments method

--- src/reddit_client/client.py
+++ src/reddit_client/client.py
@@ -1,8 +1,9 @@
 from typing import Optional
 import httpx
 
-from src._types import RedditResponse
-from src.model import ThreadClean
+from src._types import RedditThreadResponse
+from src._types._comments import RedditCommentResponse
+from src.model import Comment, ThreadClean
 from src.logger import logger
 
 
@@ -12,8 +13,11 @@ class RedditClient:
     def __init__(self, client: Optional[httpx.Client] = None) -> None:
         self._http = client or httpx.Client()
 
-    def _build_url(self, subreddit: str) -> str:
+    def _build_thread_url(self, subreddit: str) -> str:
         return f"{self.base_url}/r/{subreddit}/new.json"
+
+    def _build_comments_url(self, subreddit: str) -> str:
+        return f"{self.base_url}/r/{subreddit}/comments.json"
 
     @property
     def _headers(self) -> dict:
@@ -33,7 +37,7 @@ class RedditClient:
         }
         return headers
 
-    def _process_threads(self, response: RedditResponse):
+    def _process_threads(self, response: RedditThreadResponse):
         threads = response["data"]["children"]
         return [
             ThreadClean(
@@ -42,15 +46,42 @@ class RedditClient:
                 selftext=thread["data"]["selftext"],
                 created=thread["data"]["created"],
                 author=thread["data"]["author"],
+                comments=thread["data"]["num_comments"],
+                upvotes=thread["data"]["ups"],
+                permalink=thread["data"]["permalink"],
             )
             for thread in threads
         ]
 
+    def _process_comments(self, response: RedditCommentResponse):
+        comments = response["data"]["children"]
+        return [
+            Comment(
+                id=comment["id"],
+                thread_id=comment["parent_id"].removeprefix("t3_"),
+                permalink=comment["permalink"],
+                upvotes=comment["ups"],
+                text=comment["body"],
+                created=comment["created"],
+                author=comment["author"],
+            )
+            for comment in comments
+        ]
+
     def fetch_threads_for_subreddit(self, subreddit: str, limit: int = 100):
-        url = self._build_url(subreddit=subreddit)
+        url = self._build_thread_url(subreddit=subreddit)
         logger.info(f"Fetching {url}")
         res = self._http.get(url=url, params={"limit": limit}, headers=self._headers)
         logger.info(f"Status: {res.status_code}")
         res.raise_for_status()
-        res_json: RedditResponse = res.json()
+        res_json: RedditThreadResponse = res.json()
         return self._process_threads(response=res_json)
+
+    def fetch_comments_for_subreddit(self, subreddit: str, limit: int = 100):
+        url = self._build_comments_url(subreddit=subreddit)
+        logger.info(f"Fetching {url}")
+        res = self._http.get(url=url, params={"limit": limit}, headers=self._headers)
+        logger.info(f"Status: {res.status_code}")
+        res.raise_for_status()
+        res_json: RedditCommentResponse = res.json()
+        return self._process_comments(response=res_json)
