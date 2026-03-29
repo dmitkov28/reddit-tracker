5bb3483562bbfb0a76ef464b910e960b230f1d73
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-29 19:43:41 +0300

    chore: add bucket lifecycle rules

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,3 +1,49 @@
 resource "aws_s3_bucket" "reddit-tracker-bucket" {
-    bucket = "rt-reddit-tracker-bucket"
+  bucket = "rt-reddit-tracker-bucket"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "reddit-tracker-bucket-lifecycle-config" {
+  bucket = aws_s3_bucket.reddit-tracker-bucket.id
+
+  rule {
+    id = "expire-raw-threads-after-14-days"
+
+    status = "Enabled"
+
+    filter {
+      prefix = "threads/"
+    }
+
+    expiration {
+      days = 14
+    }
+  }
+
+  rule {
+    id = "expire-raw-comments-after-14-days"
+
+    status = "Enabled"
+
+    filter {
+      prefix = "comments/"
+    }
+
+    expiration {
+      days = 14
+    }
+  }
+
+  rule {
+    id = "expire-athena-outputs-after-14-days"
+
+    status = "Enabled"
+
+    filter {
+      prefix = "athena/"
+    }
+
+    expiration {
+      days = 14
+    }
+  }
 }
