be4446ca9c2bba0354bb4e12b9e3c6a6c8fb4733
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-30 10:00:46 +0300

    chore: update ecr lifecycle policy

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -10,17 +10,28 @@ module "reddit-tracker-ecr" {
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last 3 tagged images",
+        description  = "Keep latest tag forever (1 max)",
         selection = {
           tagStatus     = "tagged",
-          tagPrefixList = ["v", "latest", "main", "dev", "prod", "sha"],
+          tagPrefixList = ["latest"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 1
+        },
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2,
+        description  = "Keep last 3 sha- images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["sha-"],
           countType     = "imageCountMoreThan",
           countNumber   = 3
         },
         action = { type = "expire" }
       },
       {
-        rulePriority = 2,
+        rulePriority = 3,
         description  = "Delete untagged images after 1 day",
         selection = {
           tagStatus   = "untagged",
