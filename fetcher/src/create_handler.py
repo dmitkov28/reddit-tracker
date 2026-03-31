559ba4dd62999b60c78df1b261cca7b4a3381d47
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-31 13:24:22 +0300

    refactor: clean up handler code by using services

--- fetcher/src/create_handler.py
+++ fetcher/src/create_handler.py
@@ -2,12 +2,13 @@ import os
 
 import httpx
 
+from src.services.subreddits import collect_subreddit
+from src.services.comments import collect_comments
+from src.services.threads import collect_threads
 from src import RedditClient
-from src.model import CommentList
 from src.storer import S3Storer
 from src.storer.storer import Storer
-from src.utils.date_utils import Today
-from src.utils.s3 import S3, CommentPartition, ThreadPartition
+from src.utils.s3 import S3
 from src.logger import logger
 from src.utils.str_utils import clean_subreddit
 
@@ -22,43 +23,23 @@ def create_handler(storer: Storer = S3Storer(S3(bucket=BUCKET))):
     def handler(event: dict, context: dict):
         subreddit = clean_subreddit(event["subreddit"])
 
-        today = Today()
-
         with httpx.Client(timeout=30, proxy=PROXY, follow_redirects=True) as client:
             client = RedditClient(client=client)
 
             try:
-                comments = client.fetch_comments_for_subreddit(subreddit=subreddit)
-                threads = client.fetch_threads_for_subreddit(subreddit=subreddit)
-                for thread in threads:
-                    storer.store(
-                        partition=ThreadPartition(
-                            subreddit=subreddit,
-                            year=today.year,
-                            month=today.month,
-                            day=today.day,
-                            thread_id=thread.id,
-                        ),
-                        data=thread,
-                    )
-                    comments_for_thread = [
-                        comment
-                        for comment in comments
-                        if comment.thread_id == thread.id
-                    ]
-                    comment_list = CommentList(data=comments_for_thread)
-                    if comment_list.data:
-                        storer.store(
-                            partition=CommentPartition(
-                                subreddit=subreddit,
-                                thread_id=thread.id,
-                                year=today.year,
-                                month=today.month,
-                                day=today.day,
-                            ),
-                            data=comment_list,
-                        )
+                threads = collect_threads(
+                    reddit_client=client, storer=storer, subreddit=subreddit
+                )
+                collect_comments(
+                    reddit_client=client,
+                    storer=storer,
+                    threads=threads,
+                    subreddit=subreddit,
+                )
 
+                collect_subreddit(
+                    reddit_client=client, storer=storer, subreddit=subreddit
+                )
             except Exception as e:
                 logger.exception(str(e))
 
