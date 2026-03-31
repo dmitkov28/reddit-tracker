e3018ed617e9cb9a41e0b9618642468bdff94fbf
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-03-31 22:12:52 +0300

    feat: add subreddits table

--- infrastructure/glue.tf
+++ infrastructure/glue.tf
@@ -177,3 +177,65 @@ resource "aws_glue_catalog_table" "reddit-comments" {
     "EXTERNAL"                  = "TRUE"
   }
 }
+
+
+resource "aws_glue_catalog_table" "subreddits" {
+  name          = "subreddits"
+  database_name = aws_glue_catalog_database.reddit.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  partition_keys {
+    name = "year"
+    type = "int"
+  }
+
+  partition_keys {
+    name = "month"
+    type = "int"
+  }
+
+  partition_keys {
+    name = "day"
+    type = "int"
+  }
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.reddit-tracker-bucket.bucket}/subreddits/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+    }
+
+    columns {
+      name = "id"
+      type = "string"
+    }
+
+    columns {
+      name = "name"
+      type = "string"
+    }
+
+    columns {
+      name = "subscribers"
+      type = "int"
+    }
+
+
+  }
+
+  parameters = {
+    "projection.enabled"        = "true"
+    "projection.year.type"      = "integer"
+    "projection.year.range"     = "2025,2050"
+    "projection.month.type"     = "integer"
+    "projection.month.range"    = "1,12"
+    "projection.day.type"       = "integer"
+    "projection.day.range"      = "1,31"
+    "storage.location.template" = "s3://${aws_s3_bucket.reddit-tracker-bucket.bucket}/subreddits/year=$${year}/month=$${month}/day=$${day}/"
+    "EXTERNAL"                  = "TRUE"
+  }
+}
