fa1b3dd26c437163a4a815258a1f5a208a233a9d
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-04-01 13:16:22 +0300

    feat: add transformer function

---
+++ transformer/main.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+import logging
+import os
+
+import boto3
+
+SUBREDDITS_QUERY = """
+SELECT 
+    id,
+    name,
+    subscribers
+FROM subreddits
+WHERE year = year(CURRENT_DATE)
+  AND month = month(CURRENT_DATE)
+  AND day = day(CURRENT_DATE);
+"""
+
+COMMENTS_QUERY = """
+SELECT 
+    id,
+    thread_id,
+    text,
+    author,
+    permalink,
+    upvotes,
+    downvotes,
+    CAST(from_unixtime(created) AS DATE) AS created_date
+FROM comments
+WHERE year = year(CURRENT_DATE)
+  AND month = month(CURRENT_DATE)
+  AND day = day(CURRENT_DATE);
+"""
+
+THREADS_QUERY = """
+SELECT 
+    id,
+    title,
+    selftext AS text,
+    author,
+    permalink,
+    comments,
+    upvotes,
+    downvotes,
+    CAST(from_unixtime(created) AS DATE) AS created_date
+FROM reddit.threads
+WHERE year = year(CURRENT_DATE)
+  AND month = month(CURRENT_DATE)
+  AND day = day(CURRENT_DATE);
+"""
+
+
+logger = logging.getLogger()
+logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
+
+
+def lambda_handler(event: dict, context: dict):
+    DATABASE = os.environ["ATHENA_DB"]
+    BUCKET = os.environ["BUCKET"]
+
+    logger.info("Starting transformation", extra={"bucket": BUCKET})
+
+    DATE_PREFIX = datetime.now().strftime("%d-%m-%Y")
+
+    OUTPUT = f"s3://{BUCKET}/athena/{DATE_PREFIX}"
+
+    athena_client = boto3.client("athena", region_name="eu-central-1")
+
+    queries = [
+        ("subreddits", SUBREDDITS_QUERY),
+        ("comments", COMMENTS_QUERY),
+        ("threads", THREADS_QUERY),
+    ]
+
+    for name, query in queries:
+        response = athena_client.start_query_execution(
+            QueryString=query,
+            QueryExecutionContext={"Database": DATABASE},
+            ResultConfiguration={"OutputLocation": OUTPUT},
+        )
+        query_id = response["QueryExecutionId"]
+        logger.info(
+            "Query started",
+            extra={"table": name, "query_execution_id": query_id},
+        )
