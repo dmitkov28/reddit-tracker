61a601730e99caa10f71b24da28ce3eeab6f413f
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-04-01 16:45:22 +0300

    feat: add eventbridge rule to call dispatcher function on a schedule

---
+++ infrastructure/eventbridge.tf
@@ -0,0 +1,26 @@
+module "eventbridge" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  version = "4.3.0"
+
+  create_bus           = false
+  attach_lambda_policy = true
+  lambda_target_arns = [
+    module.dispatcher-lambda-function.lambda_function_arn
+  ]
+  rules = {
+    schedule = {
+      description         = "Trigger every day"
+      schedule_expression = "rate(1 day)"
+      enabled             = true
+    }
+  }
+
+  targets = {
+    schedule = [
+      {
+        name = "Dispatcher Lambda"
+        arn  = module.dispatcher-lambda-function.lambda_function_arn
+      }
+    ]
+  }
+}
