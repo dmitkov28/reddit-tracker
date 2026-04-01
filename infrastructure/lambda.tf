fd86ae9ebb5134c5eeb11fa4392a7e253619b80c
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-04-01 13:35:30 +0300

    feat: add transformer lambda

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -100,3 +100,70 @@ module "dispatcher-lambda-function" {
 
   tags = { "Terraform" : true }
 }
+
+module "transformer-lambda-function" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "8.7.0"
+
+  function_name = "reddit-tracker-transformer"
+  description   = "Reddit Tracker Transformer"
+  architectures = ["arm64"]
+
+  create_package = true
+  source_path    = "../transformer/main.py"
+  trigger_on_package_timestamp = false
+  
+  memory_size    = 128
+  timeout        = 15
+
+  runtime = "python3.13"
+  handler = "main.lambda_handler"
+
+  environment_variables = {
+    "BUCKET"    = aws_s3_bucket.reddit-tracker-bucket.bucket
+    "ATHENA_DB" = aws_glue_catalog_database.reddit.name
+  }
+
+  package_type = "Zip"
+
+  cloudwatch_logs_retention_in_days = 5
+
+  attach_policy_statements = true
+  policy_statements = {
+    bucket_read = {
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:PutObject"
+      ]
+      resources = [
+        aws_s3_bucket.reddit-tracker-bucket.arn,
+        "${aws_s3_bucket.reddit-tracker-bucket.arn}/*"
+      ]
+    }
+    glue_read = {
+      effect = "Allow"
+      actions = [
+        "glue:GetTable",
+        "glue:GetDatabase",
+        "glue:GetPartitions"
+      ]
+      resources = [
+        aws_glue_catalog_database.reddit.arn
+      ]
+    }
+    athena_query = {
+      effect = "Allow"
+      actions = [
+        "athena:StartQueryExecution"
+      ]
+      resources = [
+        "*"
+      ]
+    }
+
+  }
+
+  tags = { "Terraform" : true }
+}
