0ef58c8f4e48d4340f96b7191656c02988bdeb67
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-04-02 22:15:03 +0300

    fix: add AWS Lambda permission for EventBridge invocation

--- infrastructure/eventbridge.tf
+++ infrastructure/eventbridge.tf
@@ -1,3 +1,11 @@
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = module.dispatcher-lambda-function.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = module.eventbridge.eventbridge_rule_arns["schedule"]
+}
+
 module "eventbridge" {
   source  = "terraform-aws-modules/eventbridge/aws"
   version = "4.3.0"
