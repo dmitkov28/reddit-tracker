5e1094551f911d1b0fd14081fd81fadf4e292c6d
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-04-10 11:20:12 +0300

    chore: clean up s3 config

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -62,42 +62,5 @@ resource "aws_s3_bucket_lifecycle_configuration" "reddit-tracker-bucket-lifecycl
   }
 }
 
-data "aws_iam_policy_document" "assume-role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam-for-lambda" {
-  name               = "iam-for-transformer-function"
-  assume_role_policy = data.aws_iam_policy_document.assume-role.json
-}
-
-resource "aws_lambda_permission" "allow-bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = module.transformer-lambda-function.lambda_function_arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.reddit-tracker-bucket.arn
-}
 
 
-resource "aws_s3_bucket_notification" "bucket-notification" {
-  bucket = aws_s3_bucket.reddit-tracker-bucket.id
-
-  lambda_function {
-    lambda_function_arn = module.transformer-lambda-function.lambda_function_arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "_DONE"
-    filter_suffix       = "_DONE"
-  }
-
-  depends_on = [aws_lambda_permission.allow-bucket]
-}
